5c4513a1a8c2588f7154bb6625dd14c815b6ea59
AlexKlum <alexanderklum@gmail.com>
2024-04-06 21:36:22 +0200

    first commit

---
+++ wordpress/wp-content/themes/Bokhandel/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alexanders Bokhandel</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <script type="text/javascript" src="functions.php"></script>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+
+<div class="header-container">
+    <nav class="header-menu">
+        <a class="menu-btn" href="http://localhost:8400/homepage/">HEM</a>
+        <a class="menu-btn" href="http://localhost:8400/bokhandeln/">BOKHANDELN</a>
+        <a class="menu-btn" href="http://localhost:8400/om-oss/">OM OSS</a>
+    </nav>
+
+    <a class="kundvagn-btn" href="http://localhost:8400/cart/"><img src="http://localhost:8400/wp-content/uploads/2023/06/cart1.jpg" alt="Till kundvagn"></a>
+</div>
